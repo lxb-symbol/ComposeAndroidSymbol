6b39ac4ccee51907d2d08ba1666bb5d4f4a51be3
symbol <lxb.symbol@gmail.com>
2023-09-21 10:45:33 +0800

    1. 自行体验 Column 控件 2. 体验 Row 控件

--- app/src/main/java/com/example/compose_android_symbol/MainActivity.kt
+++ app/src/main/java/com/example/compose_android_symbol/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.compose_android_symbol
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose_android_symbol.compose_basic.MyTextRow
+import com.example.compose_android_symbol.ui.theme.COLOR_EF
 import com.example.compose_android_symbol.ui.theme.ComposeAndroidsymbolTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,6 +42,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+            .padding(horizontal = 12.dp, vertical = 10.dp)
+            .background(color = Color.Green, shape = RoundedCornerShape(4.dp))
     )
 }
 
@@ -41,6 +51,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeAndroidsymbolTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .background(color = COLOR_EF)
+        ) {
+            Greeting("Android")
+            Greeting("Android")
+            MyTextRow()
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/example/compose_android_symbol/compose_basic/Widgets.kt
@@ -0,0 +1,58 @@
+package com.example.compose_android_symbol.compose_basic
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose_android_symbol.ui.theme.COLOR_F6F6F6
+
+
+@Composable
+fun MyTextRow(
+    list: List<String> = listOf("1", "2", "3")
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(16.dp)
+            .background(color = COLOR_F6F6F6),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        for (str in list) {
+            Box(
+                modifier = Modifier
+                    .width(46.dp)
+                    .height(46.dp)
+                    .background(Color.Cyan)
+                    .border(border = BorderStroke(width = 1.dp, Color.Green)),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = str,
+                    fontFamily = FontFamily.Serif,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
+}
