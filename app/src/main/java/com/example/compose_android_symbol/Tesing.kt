279edce5fc26b11c7798e08d6f728dc24b1830ed
symbol <lxb.symbol@gmail.com>
2023-09-21 09:32:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/compose_android_symbol/Tesing.kt
@@ -0,0 +1,11 @@
+package com.example.compose_android_symbol
+
+class Tesing {
+
+    fun hell(msg:String){
+        println(msg)
+            println("给话")
+
+                println("WHAT")
+    }
+}
---
+++ mylibrary/src/main/java/com/symbol/mylibrary/Hello.java
@@ -0,0 +1,9 @@
+package com.symbol.mylibrary;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("this");
+            System.out.println("this");
+    }
+}
